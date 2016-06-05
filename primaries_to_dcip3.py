24257f4e218a1b7ad9261abd2f16a80bb3fc079f
bsloan666 <bsloan@pixelmonks.com>
2016-06-05 14:14:20 -0700

    added comments and json input

--- primaries_to_dcip3.py
+++ primaries_to_dcip3.py
@@ -33,16 +33,38 @@ import json
 import math
 from optparse import OptionParser
 
+def parse_cmdline():
+    parser = OptionParser()
+    
+    parser.add_option("-i", "--input", dest="infname",
+                      help="Read left and right primaries and white point from json file")
+    parser.add_option("-o", "--output", dest="outfname",
+                      help="Write left and right compensation matrices to json file")
+    parser.add_option("-t", "--test", dest="test", action="store_false",
+                      help="Use dummy left/right primaries to test the program")
+
+    return parser.parse_args()
+
 def promote_xyz(a):
+    """
+    Add a 'z' coordinate to each xy pair
+    """
     for p in a:
         p.append(1.0-p[0]-p[1])
     return a
 
 def xyz_to_XYZ(v):
+    """
+    convert xyz to XYZ
+    """
     V = [(1.0/v[1])*v[0], (1.0/v[1])*v[1],(1.0/v[1])*v[2]]
     return V
 
 def primaries_to_XYZ(b):
+    """
+    Linear algebra bit. 
+    Reference: http://www.ryanjuckett.com/programming/rgb-color-space-conversion/
+    """
     c = b[0:3]
     bb = np.matrix(c)
     d  =  bb.T.I
@@ -52,6 +74,9 @@ def primaries_to_XYZ(b):
     return bb.T * diag_w 
 
 def a_to_b(a, b):
+    """
+    Make a colorspace conversion matrix between two sets of primaries.
+    """
     a_to_xyz = primaries_to_XYZ(a)
     b_to_xyz = primaries_to_XYZ(b)
     return b_to_xyz.I * a_to_xyz
@@ -59,6 +84,7 @@ def a_to_b(a, b):
 
 if __name__ in "__main__":
 
+    # To test the program, run with --test 
     test_dcip3_rgbw =  [[0.68, 0.32], [0.265, 0.69], [0.15,0.06], [0.314, 0.351]]
     test_left_rgbw =  [[0.62, 0.34], [0.31, 0.54], [0.18,0.07], [0.3201, 0.314]]
     test_right_rgbw = [[0.65, 0.32], [0.29, 0.61], [0.17,0.04], [0.3100, 0.332]]
@@ -69,7 +95,19 @@ if __name__ in "__main__":
     left_scale =  0.88871
     right_scale = 0.97961
 
+    options,args = parse_cmdline()
 
+    if options.infname != '':
+        handle = open(options.infname, 'r')
+        data = json.load(handle)
+        handle.close()
+        test_left_rgbw = data['left_primaries_and_wp']
+        test_right_rgbw = data['right_primaries_and_wp']
+
+        left_scale =  data['left_scale'] 
+        right_scale = data['right_scale'] 
+
+    # Let's add the 'z' component
     left = promote_xyz(test_left_rgbw)
     right = promote_xyz(test_right_rgbw)
     p3 = promote_xyz(test_dcip3_rgbw)
